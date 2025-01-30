b3ca66d7d3e83424547c03f3b1babbd5ae383d26
radhesha <radhesha@techflux.in>
2025-01-30 12:41:04 +0530

    vbjh

--- src/Pages/ProfilePage/BookingTabs/UpcomingTab/UpcomingTab.js
+++ src/Pages/ProfilePage/BookingTabs/UpcomingTab/UpcomingTab.js
@@ -527,10 +527,6 @@ const UpcomingTab = () => {
         </span>
       ))}
 </p>
-<<<<<<< HEAD
-=======
-
->>>>>>> 709bf16990576467b9074f96a2e159c1ffa5efe8
                       </div>
                       {bookingsIdWise?.sub_category_id !== 9 && (
                       <div className="info-group">
